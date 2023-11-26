cb6c70433ea5630adef136bdd9e7e1bde7aa45ec
Naveenkumar153 <naveenkumartech2025@gmail.com>
2023-11-26 18:06:17 +0530

    shallow&deep | refactor: code changed

--- LinkedList/shallow-vs-deep.js
+++ LinkedList/shallow-vs-deep.js
@@ -2,10 +2,10 @@
  * Shallow copy vs deep copy
  */
 
-// const obj1 = { name:'hello', arr:['a','b'] };
-// const obj2 = { ...obj1 }; // shallow copy
-// const obj2 = JSON.parse(JSON.stringify(obj1)); // deep copy
-// obj2.name = 'hi'
-// obj2.arr.push('c');
-// console.log(obj1);
-// console.log(obj2);
+let obj1 = { name:'hello', arr:['a','b'] };
+let obj2 = { ...obj1 }; // shallow copy
+let obj3 = JSON.parse(JSON.stringify(obj1)); // deep copy
+obj2.name = 'hi'
+obj2.arr.push('c');
+console.log(obj1);
+console.log(obj2);

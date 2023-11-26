1fcebfaaff9095fed4d1d4d3f4edb55888fc9c02
Naveenkumar153 <naveenkumartech2025@gmail.com>
2023-11-26 17:58:43 +0530

    shallow&deep | feat: way of createing shallow and deep copy

---
+++ LinkedList/shallow-vs-deep.js
@@ -0,0 +1,11 @@
+/**
+ * Shallow copy vs deep copy
+ */
+
+// const obj1 = { name:'hello', arr:['a','b'] };
+// const obj2 = { ...obj1 }; // shallow copy
+// const obj2 = JSON.parse(JSON.stringify(obj1)); // deep copy
+// obj2.name = 'hi'
+// obj2.arr.push('c');
+// console.log(obj1);
+// console.log(obj2);

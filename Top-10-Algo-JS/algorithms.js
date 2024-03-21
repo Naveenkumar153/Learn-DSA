4e1a08770eabd8cdd4c52a796c5c2b81f117dedf
Naveenkumar153 <naveenkumartech2025@gmail.com>
2024-03-21 08:14:33 +0530

    Reverse string

---
+++ Top-10-Algo-JS/algorithms.js
@@ -0,0 +1,28 @@
+/**
+ * Reverse the string representation of an array  
+ * Examples: reverse('hi') => 'ih';
+ * reverse('hello') => 'olleh';
+ */
+ 
+// function reverseString(str){
+//     let reversed = '';
+//     // for(let i = str.length - 1; i >= 0; i--){
+//     //     reversed += str[i];
+//     // }
+//     // return reversed;
+
+//     // for(let i = 0; i < str.length; i++){
+//     //     reversed = str[i] + reversed;
+//     // };
+
+//     for(let char of str){
+//         console.log('Reversed',char + reversed);
+//         reversed = char + reversed;
+//     }
+
+//     return reversed;
+// };
+
+// console.log('reverse', reverseString('hello'));
+
+
